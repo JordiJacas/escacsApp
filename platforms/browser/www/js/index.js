e4211eb7f6e6296428c5bab90d2eabfbd9fad45d
Jordi <jjacasventura@iesesteveterradas.cat>
2018-04-10 19:06:55 +0200

    Commit

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,67 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    crearTablero: function() {
+        var tablero = $('#tablero');
+
+        for(var i=0;i<=8;i++){
+            for(var j=0;j<=8;j++){
+                
+                if(i == 0 && j ==0){
+                    tablero.append("<th></th>");
+                }else if(i == 0){
+                    tablero.append("<th>"+j+"</th>");
+                }else if(j == 0){
+                    
+                    tablero.append("<tr></tr> ");
+                    tablero.append("<th>"+i+"</th>");
+                }else{
+                    
+                    if(j > 8){
+                        
+                        tablero.append("<tr></tr>");
+                    }else{
+                        if((i % 2 == 0 && j % 2 == 0)||(i % 2 != 0 && j % 2 != 0)){
+                            var cela = $("<td id="+i+j+" class='whire'></td>");    
+                        }else{
+                            var cela = $("<td id="+i+j+" class='black'></td>");
+
+                        }
+                        cela.click(myFunction);
+                        tablero.append(cela);
+                    }
+                }
+            }
+        }   
+    }
+};
+
+app.initialize();
+app.crearTablero();
+
+function myFunction(event){
+    alert(event.target.id);
+}
