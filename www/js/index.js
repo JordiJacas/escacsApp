56f6db41d81bf198d63a69739e94c391caaaa0bd
Jordi <jjacasventura@iesesteveterradas.cat>
2018-04-13 20:08:17 +0200

    cosas

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,24 @@
-var torre = "♖";
-var tpos = 18;
-
-var app = {
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ var token;
+ var text;
+ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -25,74 +42,67 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
+        crearTablero();
 
-    crearTablero: function() {
-        var tablero = $('#tablero');
+        $("form").submit(function(e){
+            e.preventDefault();
+            var email = $(this).find("input[name='email']").val();
+            var password = $(this).find("input[name='password']").val();
 
-        for(var i=0;i<=8;i++){
-            for(var j=0;j<=8;j++){
-                
-                if(i == 0 && j ==0){
-                    tablero.append("<th></th>");
-                }else if(i == 0){
-                    tablero.append("<th>"+j+"</th>");
-                }else if(j == 0){
-                    tablero.append("<tr></tr> ");
-                    tablero.append("<th>"+i+"</th>");
-                }else{
-                    
-                    if(j > 8){
-                        tablero.append("<tr></tr>");
-                    }else{
-                        if((i % 2 == 0 && j % 2 == 0)||(i % 2 != 0 && j % 2 != 0)){
-                            var cela = $("<td id="+i+j+" class='white'></td>");    
-                        }else{
-                            var cela = $("<td id="+i+j+" class='black'></td>");
+            $.getJSON("https://escacsjordi.herokuapp.com/api/usuarios/login?email="+email+"&password="+password,function(data) {
+                text = $("#text");
+                text.text(data.mensaje);
+                token = data.token;
+                console.log(data.token);
+                conectado();
 
-                        }
-                        cela.click(mov);
-                        tablero.append(cela);
-                    }
-                }
-            }
-        }   
+            }).fail( function(e) {
+                alert("error");
+                console.log(e);
+            });
+        });
+
+        $('#logout').click(function(e){
+            e.preventDefault();
+            $.getJSON("https://escacsjordi.herokuapp.com/api/usuarios/logout?token="+token,function(data) {
+                text = $("#text");
+                text.text(data.mensaje);
+                token;
+            })
+        });
     }
 };
 
-app.initialize();
-app.crearTablero();
 
-function mov(event){
-    alert(event.target.id);
+function conectado(){
+    $.getJSON("https://escacsjordi.herokuapp.com/api/usuarios/conectados?token="+token,function(data) {
+                console.log(data);
+            })
 }
 
-var url = "http://localhost:8080";
-var user = {email: "user@gmail.com",password: "123456"};
-var token;
 
-var api = {
-    login: function(){
-        $.getJSON(url+"/api/usuarios/login?email="+user["email"]+"&password="+user["password"],function(data) {
-            alert("token="+data["token"]);
-            token = data["token"];
-        }).fail( function(e) {
-            alert(data["token"]);
-            console.log(e);
-        });
-    },
+function crearTablero(){
+    tablero = $('#tablero');
+    for(var i=0;i<=8;i++){
+        for(var j=0;j<=8;j++){ 
+            if(i == 0 && j ==0){tablero.append("<th></th>");}
+            else if(i == 0){tablero.append("<th>"+j+"</th>");}
+            else if(j == 0){
+                tablero.append("<tr></tr> ");
+                tablero.append("<th>"+i+"</th>");
+            }else{
+                if(j > 8){tablero.append("<tr></tr>");}
+                else{
+                    if((i % 2 == 0 && j % 2 == 0)||(i % 2 != 0 && j % 2 != 0)){var cela = $("<td id="+i+j+" class='white'></td>");}
+                    else{var cela = $("<td id="+i+j+" class='black'></td>");}
+                    tablero.append(cela);
+                }
+            }
+        }
+    }
+}
 
-    logout: function(){
-        $.getJSON(url+"/api/usuarios/logout?token="+token,function(data) {
-            alert(data["mensaje"]);
-        }).fail( function(e) {
-            alert(data["mensaje"]);
-            console.log(e);
-        });
-    },
-};
+app.initialize();
 
-$('#login').click(api.login);
-$('#logout').click(api.logout);
 
 
